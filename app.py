c31a3857aff0c672ff09c794b26e315f214b469f
rana22 <ambar.rana123@gmail.com>
2026-01-08 13:43:07 -0500

    manifest.json

---
+++ app.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from shiny import App, ui, render, reactive
+
+OMERO_LOGIN_URL = "https://nife-dev.cancer.gov/webclient/login/"
+OMERO_IFRAME_URL = "https://nife-dev.cancer.gov/webgateway/render_thumbnail/11422"
+
+WWW_DIR = Path(__file__).parent / "www"
+
+app_ui = ui.page_fluid(
+    ui.tags.div(id="omero_container"),
+    # ui.tags.script(src="omero.js")  # this will load from WWW_DIR
+    ui.tags.script(src="https://cdn.jsdelivr.net/gh/CBIIT/omero-external-ui@latest/shiny/OmeroViewer.js")
+)
+
+app = App(
+    app_ui,
+    server=lambda i, o, s: None,
+    static_assets=WWW_DIR
+)
